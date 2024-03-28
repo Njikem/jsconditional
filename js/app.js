95420eca1a4ab5c09d7789fb801ca3984e785938
Njikem <123434988+Njikem@users.noreply.github.com>
2024-03-27 20:36:14 -0600

    conditionals

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+let userPoints = 99;
+
+
+if(userPoints >= 75){
+    console.log('congratulations');
+}
+
+
+
+let isMember = true;
+
+if(isMember === true && userPoints >= 90){
+    console.log('congrats');
+}else if(isMember === false || userPoints <10){
+    console.log('Please leave')
+}else{
+    console.log('Sorry, not like that');
+}
+
+
+let userName = 'Mau';
+
+if(userName === 'Mau'){
+  console.log('Hello Mau');
+
+}else{
+    console.log('Not my name');
+}
+
+
+
+
+
+
+
